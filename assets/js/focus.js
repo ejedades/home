b33c995002e91f40d48aa955866e4df361114208
ejedades <102756772+ejedades@users.noreply.github.com>
2024-08-31 00:55:35 +0800

    create focus.js

---
+++ assets/js/focus.js
@@ -0,0 +1,11 @@
+// assets/js/focus.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Select the search input element
+    var searchInput = document.querySelector('.search-text');
+
+    // Set focus on the search input field
+    if (searchInput) {
+        searchInput.focus();
+    }
+});
